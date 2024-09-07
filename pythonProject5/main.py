28367f088035fbbb1edfc0916d4c33ee47a13e34
anapr2016 <an.apr2016@yandex.ru>
2024-09-07 19:42:28 +0300

    Add files via upload

---
+++ pythonProject5/main.py
@@ -0,0 +1,19 @@
+print(12+89-1)# This
+print('1st program', 9**0.5*5)
+print('2nd program', 9.99 > 9.98 and 1000 != 1000.1)
+print('3rd program', 2*2+2 == 2*(2+2))
+print('4th program', '123.456',(123.456*10))
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
